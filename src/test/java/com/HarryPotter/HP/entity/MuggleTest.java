8f07b2ffe20dbbf6a9e25257039e4752682ac9bf
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-05-06 13:49:32 +0200

    test: ERROR test

---
+++ src/test/java/com/HarryPotter/HP/entity/MuggleTest.java
@@ -0,0 +1,34 @@
+package com.HarryPotter.HP.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class MuggleTest {
+
+    Muggle muggle1 = new Muggle("Frank", "Longbottom", (byte) 45, false,  "Auror", true);
+
+    @Test
+    public void nameNewMuggle(){
+        assertEquals("Frank", muggle1.getName());
+    }
+
+    @Test
+    public void surnameNewMuggle() {
+        assertEquals("Auror", muggle1.getWork());
+    }
+
+    @Test
+    public void changeHaveSons() {
+        muggle1.setHaveSons(false);
+        assertEquals(false, muggle1.getHaveSons());
+    }
+
+    @Test
+    public void createNewMuggle(){
+        Muggle muggle2 = new Muggle("Frank", "Longbottom", (byte) 45, false,  "Auror", false);
+        assertEquals(muggle1, muggle2);
+    }
+}
