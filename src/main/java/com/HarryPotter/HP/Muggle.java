0af5edd45501afb7a51499890a2ef51c22f1a185
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-05-06 12:09:56 +0200

    feat:add atributes, constructor, gettersÂ&setters

--- src/main/java/com/HarryPotter/HP/Muggle.java
+++ src/main/java/com/HarryPotter/HP/Muggle.java
@@ -2,4 +2,28 @@ package com.HarryPotter.HP;
 
 public class Muggle extends Character {
 
+    protected String work;
+    protected Boolean haveSons;
+
+    public Muggle(String name, byte age, boolean alive, String work, Boolean haveSons) {
+        super(name, age, alive);
+        this.work = work;
+        this.haveSons = haveSons;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+    public void setWork(String work) {
+        this.work = work;
+    }
+
+    public Boolean getHaveSons() {
+        return haveSons;
+    }
+
+    public void setHaveSons(Boolean haveSons) {
+        this.haveSons = haveSons;
+    }
 }
