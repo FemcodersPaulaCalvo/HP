d86245051f3b2b516b84cec6befe8317039ae11a
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-05-06 11:53:54 +0200

    feat: Acreate class

---
+++ src/main/java/com/HarryPotter/HP/Character.java
@@ -0,0 +1,37 @@
+package com.HarryPotter.HP;
+
+public class Character {
+    String name;
+    byte age;
+    boolean alive;
+
+    public Character(String name, byte age, boolean alive) {
+        this.name = name;
+        this.age = age;
+        this.alive = alive;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+}
